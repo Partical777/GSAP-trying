e28d94cd2a25cf61a8a08ffbaeaebf2813c5f950
Partical777 <z25993021@gmail.com>
2019-09-15 15:27:55 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-xjhsoz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-xjhsoz)
---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { TimelineMax } from 'gsap';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+   menu = new TimelineMax({paused:true, reversed:true});
+
+  ngOnInit(){
+    this.createMenuAnim()
+  }
+
+  createMenuAnim(){
+    this.menu.to("#topLine", .5, {rotation:'45', ease:"Expo.easeInOut"},0)
+    this.menu.to("#midLine", .5, {opacity:'0', ease:"Expo.easeInOut"},0)
+    this.menu.to("#botLine", .5, {rotation:'-45', ease:"Expo.easeInOut"},0)
+  }
+  
+  menuClick() {
+    this.menu.reversed() ? this.menu.play() : this.menu.reverse();	
+    return console.log('clicked');
+   }
+}
