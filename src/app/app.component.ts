4e0161facdf110c4f3034a60ec48ed03af95f2dc
Partical777 <z25993021@gmail.com>
2019-09-17 12:37:49 +0800

    2 kind of animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,10 @@ import { TimelineMax, TweenMax } from 'gsap';
 export class AppComponent implements OnInit {
   logoArray = [0, 1];
 
-  //  menu = new TimelineMax({repeat:3, repeatDelay:1});
-  logo = new TimelineMax({repeat:-1, repeatDelay:1});
+  logo1 = new TimelineMax({repeat:-1, repeatDelay:1});
+  logo2 = new TimelineMax({repeat:-1, repeatDelay:1});
 
   ngOnInit() {
-    // this.createMenuAnim();
-    // TweenMax.from("#myID", 1, {x: 300});
   }
 
   ngAfterViewInit() {
@@ -36,7 +34,7 @@ export class AppComponent implements OnInit {
   }
 
   logoZero() {
-    this.logo
+    this.logo1
     .from(".cuteBird0", 2, {x: 75, delay:1, ease: Power4.easeOut})
     .from(".englandAndCarelineText0", 2, {x: -50, opacity: 0, ease: Power4.easeOut}, "-=1.75")
     .addLabel("redTextJumpOut0")
@@ -46,17 +44,16 @@ export class AppComponent implements OnInit {
     .to(".redTextA0", .5, {scale: 1}, "redTextBack")
     .to(".redTextB0", .5, {scale: 1}, "redTextBack")
     .from(".bottomText0", 2, {y: -20, opacity: 0, ease: Power4.easeOut});
-  }
 
-  // createMenuAnim(){
-  //   this.menu.to("#topLine", .5, {rotation:'45', ease:"Expo.easeInOut"},0)
-  //   this.menu.to("#midLine", .5, {opacity:'0', ease:"Expo.easeInOut"},0)
-  //   this.menu.to("#botLine", .5, {rotation:'-45', ease:"Expo.easeInOut"},0)
-  // }
-  
-  // menuClick() {
-  //   this.menu.reversed() ? 
-  //   (this.menu.play(), TweenMax.to("#myID", .5, {backgroundColor:"#ff0000"})) : (this.menu.reverse(), TweenMax.to("#myID", .5, {backgroundColor:"#ffffff"}));	
-  //   return console.log('clicked');
-  //  }
+    this.logo2
+    .from(".cuteBird1", 2, {x: 75, delay:1, ease: Power4.easeOut})
+    .from(".englandAndCarelineText1", 2, {x: -50, opacity: 0, ease: Power4.easeOut}, "-=1.75")
+    .addLabel("redTextJumpOut1")
+    .from(".bottomText1", 1.25, {y: -20, opacity: 0, ease: Power4.easeOut})
+    .to(".redTextA1", .5, {scale: 1.25, fill: "#C6576A", ease: Back.easeOut.config(1.7)}, "redTextJumpOut")
+    .to(".redTextB1", .5, {scale: 1.25, fill: "#C6576A", ease: Back.easeOut.config(1.7)}, "redTextJumpOut")
+    .addLabel("redTextBack")
+    .to(".redTextA1", .5, {scale: 1}, "redTextBack")
+    .to(".redTextB1", .5, {scale: 1}, "redTextBack");
+  }
 }
