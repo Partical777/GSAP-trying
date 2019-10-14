f8a972dd19adc979978529d30c82e22a8b480797
Partical777 <z25993021@gmail.com>
2019-10-14 14:42:20 +0800

    add new logo3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,17 +7,18 @@ import { TimelineMax, TweenMax } from 'gsap';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  logoArray = [0, 1];
+  logoArray = [0, 1, 2];
 
   logo1 = new TimelineMax({repeat:-1, repeatDelay:1});
   logo2 = new TimelineMax({repeat:-1, repeatDelay:1});
+  logo3 = new TimelineMax({repeat:-1, repeatDelay:1});
 
   ngOnInit() {
   }
 
   ngAfterViewInit() {
     let element = [];
-    let svgClassList = ["cuteBird", "englandAndCarelineText", "englandText", "carelineText", "redTextA", "greyText", "redTextB", "bottomText"];
+    let svgClassList = ["cuteBird", "englandAndCarelineText", "englandText", "carelineText", "redTextA", "greyText", "redTextB", "bottomText", "bomb", "bombA", "bombB", "bomb-bigA", "bomb-midA", "bomb-smallA", "bomb-bigB", "bomb-midB", "bomb-smallB"];
 
     svgClassList.forEach(function(v){
       element.push(document.getElementsByClassName(v));
@@ -55,5 +56,30 @@ export class AppComponent implements OnInit {
     .addLabel("redTextBack")
     .to(".redTextA1", .5, {scale: 1}, "redTextBack")
     .to(".redTextB1", .5, {scale: 1}, "redTextBack");
+    
+    this.logo3
+    .from(".cuteBird2", 2, {x: 75, delay:1, ease: Power4.easeOut})
+    .from(".englandAndCarelineText2", 2, {x: -50, opacity: 0, ease: Power4.easeOut}, "-=1.75")
+    .addLabel("redTextJumpOut2")
+    .from(".bottomText2", 1.25, {y: -20, opacity: 0, ease: Power4.easeOut})
+    .to(".redTextA2", .5, {scale: 1.25, fill: "#C6576A", ease: Back.easeOut}, "redTextJumpOut2")
+    .to(".redTextB2", .5, {scale: 1.25, fill: "#C6576A", ease: Back.easeOut}, "redTextJumpOut2")
+    .to(".bombA", 0, {scale: 1.15}, "redTextJumpOut2")
+    .to(".bombB", 0, {scale: 1.15}, "redTextJumpOut2")
+    .to(".bomb-bigA2", 0, {opacity: 1}, "redTextJumpOut2")
+    .to(".bomb-midA2", .25, {opacity: 1}, "redTextJumpOut2")
+    .to(".bomb-smallA2", .5, {opacity: 1}, "redTextJumpOut2")
+    .to(".bomb-bigB2", 0, {opacity: 1}, "redTextJumpOut2")
+    .to(".bomb-midB2", .25, {opacity: 1}, "redTextJumpOut2")
+    .to(".bomb-smallB2", .5, {opacity: 1}, "redTextJumpOut2")
+    .addLabel("redTextBack", "-=.75")
+    .to(".bomb-bigA2", 0, {opacity: 0}, "redTextBack")
+    .to(".bomb-midA2", .25, {opacity: 0}, "redTextBack")
+    .to(".bomb-smallA2", .5, {opacity: 0}, "redTextBack")
+    .to(".bomb-bigB2", 0, {opacity: 0}, "redTextBack")
+    .to(".bomb-midB2", .25, {opacity: 0}, "redTextBack")
+    .to(".bomb-smallB2", .5, {opacity: 0}, "redTextBack")
+    .to(".redTextA2", .5, {scale: 1}, "redTextBack")
+    .to(".redTextB2", .5, {scale: 1}, "redTextBack");
   }
 }
